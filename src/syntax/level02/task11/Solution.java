f03fe307000c471c3857195d544cfa4a8e82a977
Durf1985 <Durf1985@mail.ru>
2020-07-14 14:37:23 +0700

    Now (task## -> task#_##) # - lection number ## - sequential number in the current level

--- src/syntax/level02/task11/Solution.java
+++ src/syntax/level02/task11/Solution.java
@@ -1,30 +1,38 @@
 package syntax.level02.task11;
 
-//Уровень 2 Лекция 5
+//Уровень 2 лекция 1
 
 /*
-Самое нужное число
-Закомментируй максимальное количество строк, чтобы на экран вывелось число 19
+Реализуем метод print
+Допиши код в методе print, чтобы он выводил на экран каждую переданную в него строку 4 раза. Каждый раз с новой строки.
+
+Например, при таком вызове:
+print("Java opens many opportunities!");
+
+На экран должно быть выведено:
+Java opens many opportunities!
+Java opens many opportunities!
+Java opens many opportunities!
+Java opens many opportunities!
 
 Требования:
-•	Программа должна выводить на экран число 19.
-•	Нельзя изменять строки с объявлением переменных.
-•	Нельзя изменять строку отвечающую за вывод в консоль.
-•	Нужно закомментировать некоторые строки и не менять остальные.
+•	Программа должна выводить текст на экран.
+•	Метод main не должен вызывать System.out.println или System.out.print.
+•	Метод print должен выводить текст на экран.
+•	Метод main должен вызвать метод print класса Solution ровно три раза.
+•	Метод print должен выводить на экран переданную строку 4 раза. Каждый раз с новой строки.
 */
-
 public class Solution {
+
     public static void main(String[] args) {
-        int x = 1;
-        int y = 0;
+        print("Java is easy to learn!");
+        print("Java is object-oriented!");
+        print("Java is platform-independent!");
+    }
 
-        y = y + 3 * x;
-//        x = x * 2;
-        x = x * 16;
-//        y = y + 2 * x;
-        y = y + x;
-
-        System.out.println(y);
+    public static void print(String s) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(s);
+        }
     }
 }
-// подбешивает от таких задач
